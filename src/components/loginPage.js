4d891c3be6c0e0f6e3d401c569dcf3bf4aad1666
Umashankar Varma M <umashankar_varma_m@dell.com>
2021-11-21 14:38:36 +0530

    Added form validation for login component

--- src/components/loginPage.js
+++ src/components/loginPage.js
@@ -1,36 +1,67 @@
 
 import React from "react";
+import { Form, Button } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css'
+import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
+import TweetAppComponent from "./tweetApp";
 
 const LoginSignUp = () => {
+  let userLoggedIn = false;
+  let isAuthenticated  = () => userLoggedIn = true;
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const onSubmit = (data) => {
+    isAuthenticated()
+  }
+
   return (
-            <><h1>Log In</h1><div>
-            <label id="userNameLbl">User Name</label>
-            </div>
-            <div>
-              <input type="text" id="userName"></input>
-            </div>
             
-            <div>
-              <label id="passwordLbl">Password</label>
-            </div>
+                  <><h1>Log In</h1>
+                  <Form onSubmit={handleSubmit(onSubmit)}>
+                    <Form.Field>
+                      <label id="userNameLbl">User Name</label>
             
-            <div>
-              <input type="password" id="password"></input>
-            </div>
+                      <input type="text" id="userName" {...register("userName", { 
+                        required: true, 
+                        maxLength: 25, 
+                        pattern: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ 
+                        })} 
+                        placeholder="Enter your username to login">
+
+                      </input>
             
-            <Link to="/user/tweets">
-              <div>
-                <button type="submit" id="submitBtn">Log In</button>
-              </div>
-            </Link>
-            <div>
-              Not registered?
-              <Link to="/user/register">
-              <button type="submit" id="signUpBtn">Sign Up</button>
-              </Link>
-            </div></>
-      );
+                    </Form.Field>
+                    {errors.userName && <p>Please enter an username</p>}
+            
+                    <Form.Field>
+            
+                      <label id="passwordLbl">Password</label>
+                      <input type="password" id="password" {...register("password", { 
+                            required: true, 
+                            pattern: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,15}$/
+                            })} 
+                            placeholder="Enter your password">
+
+                      </input>
+            
+                    </Form.Field>
+                    {errors.password && <p>Password should contain atleast a capital letter, a small letter and must be 6 chars, max 15 chars</p>}
+                    <div>
+                      <Button type="submit" id="submitBtn">Log In</Button>
+                    </div>
+            
+                  </Form>
+                  <div>
+                  <span>Not registered?</span>
+                  <Link to="/user/register">
+                  <Button type="submit" id="signUpBtn">Sign Up</Button>
+                  </Link>
+              
+                  </div></>
+              
+          );
+
+      
     };
     
     export default LoginSignUp;
