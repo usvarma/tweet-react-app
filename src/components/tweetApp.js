c8b782d725d1d30e85f82069dc4c7488e0ddd21c
Umashankar Varma M <umashankar_varma_m@dell.com>
2021-11-22 11:26:38 +0530

    Modified tweet list and added styles

--- src/components/tweetApp.js
+++ src/components/tweetApp.js
@@ -1,9 +1,19 @@
 import React from "react";
-import TweetList from "./tweetList";
+//import TweetList from "./tweetList";
+import ComposeForm from './composeform'
+import Timeline from './timeline'
+import { FaTwitter } from 'react-icons/fa'
+import '../styles/tweetapp.css'
+import tweets from '../data/tweets.json';
 
 const TweetAppComponent = () => {
   return (
-      <TweetList></TweetList>
+    <div className="app">
+    <FaTwitter className="app-logo" size="2em" />
+    <ComposeForm />
+    <div className="separator"></div>
+    <Timeline tweets={tweets} />
+  </div>
   );
 };
 
