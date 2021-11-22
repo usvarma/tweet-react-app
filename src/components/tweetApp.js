12603597205915bb3e9b1c676b6bcd127e54e45d
Umashankar Varma M <umashankar_varma_m@dell.com>
2021-11-22 19:13:47 +0530

    Updated composeform and tweet component

--- src/components/tweetApp.js
+++ src/components/tweetApp.js
@@ -1,20 +1,38 @@
 import React from "react";
-//import TweetList from "./tweetList";
-import ComposeForm from './composeform'
-import Timeline from './timeline'
-import { FaTwitter } from 'react-icons/fa'
-import '../styles/tweetapp.css'
-import tweets from '../data/tweets.json';
+import { useState } from 'react';
+import { nanoid } from 'nanoid';
+import ComposeForm from './composeform';
+import Timeline from './timeline';
+import { FaTwitter } from 'react-icons/fa';
+import '../styles/tweetapp.css';
+import initialTweets from '../data/tweets.json';
+
+const CURRENT_USER = 'twitterbot'
 
 const TweetAppComponent = () => {
+  
+  const [tweets, setTweets] = useState(initialTweets)
+  
+  const handlePostTweet = (content) => {
+                                          const newTweet = {  content, 
+                                                              id: nanoid(), 
+                                                              created_on: Date(Date.now()), 
+                                                              user: CURRENT_USER,      
+                                                              comments_count: 0,      
+                                                              retweets_count: 0,      
+                                                              favorites_count: 0,    
+                                                            }
+                                                            
+                                          setTweets([...tweets, newTweet])  
+                                        }
   return (
-    <div className="app">
-    <FaTwitter className="app-logo" size="2em" />
-    <ComposeForm />
-    <div className="separator"></div>
-    <Timeline tweets={tweets} />
-  </div>
-  );
+            <div className="app">
+              <FaTwitter className="app-logo" size="2em" />
+              <ComposeForm onSubmit={handlePostTweet} />      
+              <div className="separator"></div>
+              <Timeline tweets={tweets} />
+            </div>
+          )
 };
 
 export default TweetAppComponent;
