99608750ab2ae447a4915c7edc1baa0fc58cd1c7
Umashankar Varma M <umashankar_varma_m@dell.com>
2021-12-08 12:01:17 +0530

    Updated styles for register page

--- src/components/registerPage.js
+++ src/components/registerPage.js
@@ -1,19 +1,20 @@
 import React from "react";
+import '../styles/register.css';
 
 const RegisterComponent = () => {
     return (
         <div class="container">
             <form>
                 <div class="form-group">
-                    <label for="exampleInputEmail1">Email address</label>
+                    <label for="emailAddress">Email address</label>
                     <input type="email" class="form-control" id="registerEmail" aria-describedby="emailHelp" placeholder="Enter email"></input>
                 </div>
                 <div class="form-group">
-                    <label for="exampleInputPassword1">Password</label>
+                    <label for="Password" >Password</label>
                     <input type="password" class="form-control" id="registerPassword" placeholder="Password"></input>
                 </div>
                 <div class="form-group">
-                    <label for="exampleInputPassword1">Confirm Password</label>
+                    <label for="confirmPassword">Confirm Password</label>
                     <input type="password" class="form-control" id="registerConfirmPassword" placeholder="Confirm password"></input>
                 </div>
                 <button type="submit" class="btn btn-primary">Sign Up</button>
