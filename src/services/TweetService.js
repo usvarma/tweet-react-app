e93ea686494e9a2259754d4a873cbd01390b0a27
Umashankar Varma M <umashankar_varma_m@dell.com>
2022-03-03 14:03:24 +0530

    Updated tweetservice with api calls

---
+++ src/services/TweetService.js
@@ -0,0 +1,175 @@
+const baseUrl = `https://localhost:44319/api/v1.0/tweets`;
+const token = ""; //Add call to tokenservice to get token
+const requestHeader = {'Content-Type': 'application/json', 'Authorization':token};
+
+
+export async function getAllTweets() {
+
+    try {
+        const request = new Request(`${baseUrl}/all`, {
+            method: 'GET',
+            mode: 'cors',
+            cache: 'no-cache',
+            headers: requestHeader,
+            redirect: 'follow',
+            referrerPolicy: 'no-referrer'
+        });
+
+        const response = await fetch(request);
+        if (!response.ok) {
+            throw new Error(`${response.status}`);
+        }
+        
+        return await response.json();
+
+    } catch (error) {
+        throw error;
+    }
+}
+
+export async function getAllTweetsOfUser(username) {
+
+    try {
+        const request = new Request(`${baseUrl}/${username}`, {
+            method: 'GET',
+            mode: 'cors',
+            cache: 'no-cache',
+            headers: requestHeader,
+            redirect: 'follow',
+            referrerPolicy: 'no-referrer'
+        });
+
+        const response = await fetch(request);
+        if (!response.ok) {
+            throw new Error(`${response.status}`);
+        }
+        
+        return await response.json();
+
+    } catch (error) {
+        throw error;
+    }
+}
+
+export async function addTweetsForUser(tweetData, username) {
+
+    try {
+        const request = new Request(`${baseUrl}/${username}/add`, {
+            method: 'POST',
+            mode: 'cors',
+            cache: 'no-cache',
+            headers: requestHeader,
+            redirect: 'follow',
+            referrerPolicy: 'no-referrer',
+            body: JSON.stringify(tweetData)
+        });
+
+        const response = await fetch(request);
+        if (!response.ok) {
+            throw new Error(`${response.status}`);
+        }
+        
+        return await response.json();
+
+    } catch (error) {
+        throw error;
+    }
+}
+
+export async function updateTweetsForUser(updatedTweet, username, id) {
+
+    try {
+        const request = new Request(`${baseUrl}/${username}/update/${id}`, {
+            method: 'PUT',
+            mode: 'cors',
+            cache: 'no-cache',
+            headers: requestHeader,
+            redirect: 'follow',
+            referrerPolicy: 'no-referrer',
+            body: JSON.stringify(updatedTweet)
+        });
+
+        const response = await fetch(request);
+        if (!response.ok) {
+            throw new Error(`${response.status}`);
+        }
+        
+        return await response.json();
+
+    } catch (error) {
+        throw error;
+    }
+}
+
+export async function deleteTweetsForUser(username, id) {
+
+    try {
+        const request = new Request(`${baseUrl}/${username}/delete/${id}`, {
+            method: 'DELETE',
+            mode: 'cors',
+            cache: 'no-cache',
+            headers: requestHeader,
+            redirect: 'follow',
+            referrerPolicy: 'no-referrer'
+        });
+
+        const response = await fetch(request);
+        if (!response.ok) {
+            throw new Error(`${response.status}`);
+        }
+        
+        return await response.json();
+
+    } catch (error) {
+        throw error;
+    }
+}
+
+export async function likeTweet(username, id) {
+
+    try {
+        const request = new Request(`${baseUrl}/${username}/like/${id}`, {
+            method: 'PUT',
+            mode: 'cors',
+            cache: 'no-cache',
+            headers: requestHeader,
+            redirect: 'follow',
+            referrerPolicy: 'no-referrer'
+        });
+
+        const response = await fetch(request);
+        if (!response.ok) {
+            throw new Error(`${response.status}`);
+        }
+        
+        return await response.json();
+
+    } catch (error) {
+        throw error;
+    }
+}
+
+export async function replyForTweet(username, id, replyTweet) {
+
+    try {
+        const request = new Request(`${baseUrl}/${username}/reply/${id}`, {
+            method: 'POST',
+            mode: 'cors',
+            cache: 'no-cache',
+            headers: requestHeader,
+            redirect: 'follow',
+            referrerPolicy: 'no-referrer',
+            body: JSON.stringify(replyTweet)
+        });
+
+        const response = await fetch(request);
+        if (!response.ok) {
+            throw new Error(`${response.status}`);
+        }
+        
+        return await response.json();
+
+    } catch (error) {
+        throw error;
+    }
+}
